1e63fbc4566e28c837adc2ac6fd2eae216a89da8
Jeffrey Way <jeffrey@envato.com>
2013-04-16 11:21:46 -0400

    Fix controller tests

--- tests/generators/ControllerGeneratorTest.php
+++ tests/generators/ControllerGeneratorTest.php
@@ -19,24 +19,26 @@ class ControllerGeneratorTest extends PHPUnit_Framework_TestCase {
     public function testCanGenerateControllerUsingDefaultTemplate()
     {
         $file = m::mock('Illuminate\Filesystem\Filesystem')->makePartial();
+        $cache = m::Mock('Way\Generators\Cache');
 
         $file->shouldReceive('put')
              ->once()
              ->with('app/controllers/FooController.php', file_get_contents(__DIR__.'/stubs/controller.txt'));
 
-        $generator = new ControllerGenerator($file);
-        $generator->make('app/controllers/FooController.php', __DIR__.'/../../src/Way/Generators/Generators/templates/controller.txt');
+        $generator = new ControllerGenerator($file, $cache);
+        $generator->make('app/controllers/FooController.php', static::$templatesDir.'/controller.txt');
     }
 
     public function testCanGenerateControllerUsingCustomTemplate()
     {
         $file = m::mock('Illuminate\Filesystem\Filesystem')->makePartial();
+        $cache = m::Mock('Way\Generators\Cache');
 
         $file->shouldReceive('put')
              ->once()
              ->with('app/controllers/FoosController.php', file_get_contents(__DIR__.'/stubs/scaffold/controller.txt'));
 
-        $generator = new ControllerGenerator($file);
+        $generator = new ControllerGenerator($file, $cache);
         $generator->make('app/controllers/FoosController.php', static::$templatesDir.'/scaffold/controller.txt');
     }
 }
