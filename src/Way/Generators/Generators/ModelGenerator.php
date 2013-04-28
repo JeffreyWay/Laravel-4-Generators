663fe8a33ed41d8b8b4878ed8b7fa3e9ae29224d
Anders Persson <anders_persson_360@yahoo.se>
2013-04-28 16:02:54 +0200

    Implemented relation stuff to the model command

--- src/Way/Generators/Generators/ModelGenerator.php
+++ src/Way/Generators/Generators/ModelGenerator.php
@@ -4,6 +4,28 @@ namespace Way\Generators\Generators;
 
 class ModelGenerator extends Generator {
 
+
+	/**
+     * Parse the relation model file
+     *
+     * @param  string $name
+     * @param  string $relname
+	 * @param  string $type
+     * @param  string $separator
+	 * @param  string $container
+     */
+	protected function relText($name, $relname, $type, $separator, $container)
+	{
+		
+		$upperCase = ucfirst($relname);
+		$relname = substr($relname, 2, 0);
+		$container = str_replace('{{relname}}', $relname, $container);
+		$container = str_replace('{{reltype}}', $type, $container);
+		$container = str_replace('{{relnameUpperCase}}', $upperCase, $container);
+		
+		return str_replace('{{name}}', strstr($name, $separator, false), $container);
+	}
+
     /**
      * Fetch the compiled template for a model
      *
@@ -13,14 +35,36 @@ class ModelGenerator extends Generator {
      */
     protected function getTemplate($template, $name)
     {
-        $this->template = $this->file->get($template);
+		$hasManySign = '->';
+		$belongsToSign = '<-';
 
-        if ($this->needsScaffolding($template))
-        {
-            $this->template = $this->getScaffoldedModel($name);
-        }
-
-        return str_replace('{{name}}', $name, $this->template);
+        
+		
+		if($relname = strstr($name, $hasManySign))
+		{
+			$this->template = $this->file->get($relTemplate);
+			return relText($name, $relname, 'hasMany', $hasManySign, $this->template);
+			
+		}
+		elseif($relname = strstr($name, $belongsToSign))
+		{
+			$this->template = $this->file->get($relTemplate);
+			return relText($name, $relname, 'belongsTo', $belongsToSign, $this->template);
+			
+		}
+		else
+		{
+			if ($this->needsScaffolding($template))
+        	{
+        	    $this->template = $this->getScaffoldedModel($name);
+        	}
+			else
+			{
+				$this->template = $this->file->get($template);
+			}
+			return str_replace('{{name}}', $name, $this->template);
+		}
+        
     }
 
     /**
@@ -43,5 +87,4 @@ class ModelGenerator extends Generator {
 
         return str_replace('{{rules}}', PHP_EOL."\t\t".implode(','.PHP_EOL."\t\t", $rules) . PHP_EOL."\t", $this->template);
     }
-
 }
