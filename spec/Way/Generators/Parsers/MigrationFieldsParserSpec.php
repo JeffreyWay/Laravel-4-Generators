5dbda1f9b43a9d65309a93501069e119fe97e792
Jeffrey Way <jeffrey@jeffrey-way.com>
2014-03-03 03:23:59 -0500

    Improve functionality/parsing for migration fields

--- spec/Way/Generators/Parsers/MigrationFieldsParserSpec.php
+++ spec/Way/Generators/Parsers/MigrationFieldsParserSpec.php
@@ -15,18 +15,35 @@ class MigrationFieldsParserSpec extends ObjectBehavior {
     function it_parses_a_string_of_fields()
     {
         $this->parse('name:string')->shouldReturn([
-            'name' => 'string'
+            'name' => ['type' => 'string']
         ]);
 
         $this->parse('name:string, age:integer')->shouldReturn([
-            'name' => 'string',
-            'age'  => 'integer'
+            'name' => ['type' => 'string'],
+            'age'  => ['type' => 'integer']
         ]);
 
-        $this->parse('name:string,age : integer')->shouldReturn([
-            'name' => 'string',
-            'age'  => 'integer'
+        $this->parse('name:string, age:integer')->shouldReturn([
+            'name' => ['type' => 'string'],
+            'age'  => ['type' => 'integer']
         ]);
+
+        $this->parse('name:string:nullable, age:integer')->shouldReturn([
+            'name' => ['type' => 'string', 'decorators' => ['nullable']],
+            'age'  => ['type' => 'integer']
+        ]);
+
+        $this->parse('name:string(15):nullable')->shouldReturn([
+            'name' => ['type' => 'string', 'args' => '15', 'decorators' => ['nullable']],
+        ]);
+
+        $this->parse('column:double(15,8):nullable:default(10), age:integer')->shouldReturn([
+            'column' => ['type' => 'double', 'args' => '15,8', 'decorators' => ['nullable', 'default(10)']],
+            'age'  => ['type' => 'integer']
+        ]);
+
+
+
     }
 
 }
--- src/Way/Generators/Parsers/MigrationFieldsParser.php
+++ src/Way/Generators/Parsers/MigrationFieldsParser.php
@@ -13,12 +13,42 @@ class MigrationFieldsParser {
     {
         if ( ! $fields) return [];
 
-        $fields = preg_split('/\s?,\s?/', $fields);
+        // name:string, age:integer
+        // name:string(10,2), age:integer
+        $fields = preg_split('/\s?,\s/', $fields);
         $parsed = [];
+
         foreach($fields as $field)
         {
-            list($property, $type) = preg_split('/\s?:\s?/', $field, null);
-            $parsed[$property] = $type;
+            // Example:
+            // name:string:nullable => ['name', 'string', 'nullable']
+            // name:string(15):nullable
+            $chunks = preg_split('/\s?:\s?/', $field, null);
+
+            // The first item will be our property
+            $property = array_shift($chunks);
+
+            // The next will be the schema type
+            $type = array_shift($chunks);
+
+            $args = null;
+
+            // Sett if args were provided, like:
+            // name:string(10)
+            if (preg_match('/(.+?)\(([^)]+)\)/', $type, $matches))
+            {
+                $type = $matches[1];
+                $args = $matches[2];
+            }
+
+            // Finally, anything that remains will
+            // be our decorators
+            $decorators = $chunks;
+
+            $parsed[$property] = ['type' => $type];
+
+            if (isset($args)) $parsed[$property]['args'] = $args;
+            if ($decorators) $parsed[$property]['decorators'] = $decorators;
         }
 
         return $parsed;
