ae5f631798f2724df636ba5c1e16bc0d7607e4b3
Jeffrey Way <jeffrey@jeffrey-way.com>
2014-03-03 04:10:58 -0500

    Register commands in service provider

--- src/Way/Generators/GeneratorsServiceProvider.php
+++ src/Way/Generators/GeneratorsServiceProvider.php
@@ -1,6 +1,9 @@
 <?php namespace Way\Generators;
 
 use Illuminate\Support\ServiceProvider;
+use Way\Generators\Commands\ControllerGeneratorCommand;
+use Way\Generators\Commands\ModelGeneratorCommand;
+use Way\Generators\Commands\SeederGeneratorCommand;
 
 class GeneratorsServiceProvider extends ServiceProvider {
 
@@ -12,14 +15,75 @@ class GeneratorsServiceProvider extends ServiceProvider {
 	protected $defer = false;
 
 	/**
-	 * Register the service provider.
+	 * Register the commands
 	 *
 	 * @return void
 	 */
 	public function register()
 	{
-		//
+        foreach(['Model', 'Controller', 'Migration', 'Seeder'] as $command)
+        {
+            $this->{"register$command"}();
+        }
 	}
+
+    /**
+     * Register the model generator
+     */
+    protected function registerModel()
+    {
+        $this->app['generate.model'] = $this->app->share(function($app)
+        {
+            $generator = $this->app->make('Way\Generators\Generator');
+
+            return new ModelGeneratorCommand($generator);
+        });
+
+        $this->commands('generate.model');
+    }
+
+    /**
+     * Register the controller generator
+     */
+    protected function registerController()
+    {
+        $this->app['generate.controller'] = $this->app->share(function($app)
+        {
+            $generator = $this->app->make('Way\Generators\Generator');
+
+            return new ControllerGeneratorCommand($generator);
+        });
+
+        $this->commands('generate.controller');
+    }
+
+    /**
+     * Register the migration generator
+     */
+    protected function registerMigration()
+    {
+        $this->app['generate.migration'] = $this->app->share(function($app)
+        {
+            return $this->app->make('Way\Generators\Commands\MigrationGeneratorCommand');
+        });
+
+        $this->commands('generate.migration');
+    }
+
+    /**
+     * Register the seeder generator
+     */
+    protected function registerSeeder()
+    {
+        $this->app['generate.seeder'] = $this->app->share(function($app)
+        {
+            $generator = $this->app->make('Way\Generators\Generator');
+
+            return new SeederGeneratorCommand($generator);
+        });
+
+        $this->commands('generate.seeder');
+    }
 
 	/**
 	 * Get the services provided by the provider.
