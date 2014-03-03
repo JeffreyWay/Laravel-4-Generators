2f72e8d0b60b23665aa1c56638b494a8deb72204
Jeffrey Way <jeffrey@jeffrey-way.com>
2014-03-02 19:25:34 -0500

    Add support for controller generation

---
+++ src/Way/Generators/Laravel/ControllerGeneratorCommand.php
@@ -0,0 +1,61 @@
+<?php namespace Way\Generators\Laravel;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class ControllerGeneratorCommand extends Command {
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'generate:controller';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate a resourceful controller';
+
+    /**
+     * Generate the controller
+     */
+    public function fire()
+    {
+        // This command is nothing more than a helper,
+        // that points directly to Laravel's
+        // controller:make command
+        $this->call('controller:make', [
+            'name' => $this->argument('controllerName'),
+            '--path' => $this->option('path')
+        ]);
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return array(
+            array('controllerName', InputArgument::REQUIRED, 'The name of the desired controller')
+        );
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return array(
+            array('path', null, InputOption::VALUE_OPTIONAL, 'Where should the file be created?', app_path('controllers')),
+        );
+    }
+
+}
