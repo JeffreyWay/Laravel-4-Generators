42407064ff78ac0dc11a328a69960e983d8e38c9
Jeffrey Way <jeffrey@jeffrey-way.com>
2014-03-07 13:34:08 -0500

    Reduce duplication with inheritance

--- src/Way/Generators/Commands/ScaffoldGeneratorCommand.php
+++ src/Way/Generators/Commands/ScaffoldGeneratorCommand.php
@@ -4,7 +4,7 @@ use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 
-class ScaffoldGeneratorCommand extends Command {
+class ScaffoldGeneratorCommand extends ResourceGeneratorCommand {
 
     /**
      * The console command name.
@@ -19,74 +19,6 @@ class ScaffoldGeneratorCommand extends Command {
      * @var string
      */
     protected $description = 'Scaffold a new resource (with boilerplate)';
-
-    /**
-     * Generate a resource
-     *
-     * @return mixed
-     */
-    public function fire()
-    {
-        $resource = $this->argument('resource');
-
-        $this->callModel($resource);
-        $this->callController($resource);
-        $this->callMigration($resource);
-        $this->callSeeder($resource);
-        $this->callMigrate();
-
-        // All done!
-        $this->info(sprintf(
-            "All done! Don't forget to add '%s` to %s." . PHP_EOL,
-            "Route::resource('{$this->getTableName($resource)}', '{$this->getControllerName($resource)}');",
-            "app/routes.php"
-        ));
-
-    }
-
-    /**
-     * Get the name for the model
-     *
-     * @param $resource
-     * @return string
-     */
-    protected function getModelName($resource)
-    {
-        return ucwords(str_singular(camel_case($resource)));
-    }
-
-    /**
-     * Get the name for the controller
-     *
-     * @param $resource
-     * @return string
-     */
-    protected function getControllerName($resource)
-    {
-        return ucwords(str_plural(camel_case($resource))) . 'Controller';
-    }
-
-    /**
-     * Get the DB table name
-     *
-     * @param $resource
-     * @return string
-     */
-    protected function getTableName($resource)
-    {
-        return str_plural($resource);
-    }
-
-    /**
-     * Get the name for the migration
-     *
-     * @param $resource
-     * @return string
-     */
-    protected function getMigrationName($resource)
-    {
-        return "create_" . str_plural($resource) . "_table";
-    }
 
     /**
      * Call model generator if user confirms
@@ -122,74 +54,6 @@ class ScaffoldGeneratorCommand extends Command {
                 '--templatePath' => __DIR__.'/../templates/scaffolding/controller.txt'
             ]);
         }
-    }
-
-    /**
-     * Call migration generator if user confirms
-     *
-     * @param $resource
-     */
-    protected function callMigration($resource)
-    {
-        $migrationName = $this->getMigrationName($resource);
-
-        if ($this->confirm("Do you want me to create a '$migrationName' migration and schema for this resource? [yes|no]"))
-        {
-            $this->call('generate:migration', [
-                'migrationName' => $migrationName,
-                '--fields' => $this->option('fields')
-            ]);
-        }
-    }
-
-    /**
-     * Call seeder generator if user confirms
-     *
-     * @param $resource
-     */
-    protected function callSeeder($resource)
-    {
-        $tableName = str_plural($this->getModelName($resource));
-
-        if ($this->confirm("Would you like a '$tableName' table seeder?"))
-        {
-            $this->call('generate:seed', compact('tableName'));
-        }
-    }
-
-    /**
-     * Migrate database if user confirms
-     */
-    protected function callMigrate()
-    {
-        if ($this->confirm('Do you want to go ahead and migrate the database? [yes|no]')) {
-            $this->call('migrate');
-            $this->info('Done!');
-        }
-    }
-
-    /**
-     * Get the console command arguments.
-     *
-     * @return array
-     */
-    protected function getArguments()
-    {
-        return [
-            ['resource', InputArgument::REQUIRED, 'Singular resource name']
-        ];
-    }
-
-    /**
-     * Get the console command options.
-     *
-     * @return array
-     */
-    protected function getOptions()
-    {
-        return [
-            ['fields', null, InputOption::VALUE_OPTIONAL, 'Fields for the migration']
-        ];
     }
 
 }
