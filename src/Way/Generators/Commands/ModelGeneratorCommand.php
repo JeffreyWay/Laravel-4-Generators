9a9fc865bff1389919d98b7391f4184fd773a439
Xwiz <opatac@shoppi.ng>
2015-05-10 20:19:12 +0100

    Added fillable fields for models and resource generation

--- src/Way/Generators/Commands/ModelGeneratorCommand.php
+++ src/Way/Generators/Commands/ModelGeneratorCommand.php
@@ -2,6 +2,8 @@
 
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
+use Way\Generators\Generator;
+use Way\Generators\Parsers\MigrationFieldsParser;
 
 class ModelGeneratorCommand extends GeneratorCommand {
 
@@ -20,7 +22,20 @@ class ModelGeneratorCommand extends GeneratorCommand {
     protected $description = 'Generate a model';
 
     /**
-     * The path to where the file will be created.
+     * @param Generator $generator
+     * @param MigrationFieldsParser $migrationFieldsParser
+     */
+    public function __construct(
+        Generator $generator,
+        MigrationFieldsParser $migrationFieldsParser
+    )
+    {
+        $this->migrationFieldsParser = $migrationFieldsParser;
+        parent::__construct($generator);
+    }
+    
+    /**
+     * The path where the file will be created
      *
      * @return mixed
      */
@@ -32,20 +47,23 @@ class ModelGeneratorCommand extends GeneratorCommand {
     }
 
     /**
-     * Fetch the template data.
+     * Fetch the template data
      *
      * @return array
      */
     protected function getTemplateData()
     {
+        $fields = $this->migrationFieldsParser->parse($this->option('fillable'));
+        $padvalue = function($s){ return "'{$s['field']}'"; };
+        $paddedFields = implode(', ', array_map($padvalue, $fields));
         return [
             'NAME' => ucwords($this->argument('modelName')),
-            'NAMESPACE' => 'App'
+            'FILLABLE' => $paddedFields
         ];
     }
 
     /**
-     * Get path to the template for the generator.
+     * Get path to the template for the generator
      *
      * @return mixed
      */
@@ -62,8 +80,22 @@ class ModelGeneratorCommand extends GeneratorCommand {
     protected function getArguments()
     {
         return [
-            ['modelName', InputArgument::REQUIRED, 'The name of the desired Eloquent model']
+            ['modelName', InputArgument::REQUIRED, 'The name of the desired Eloquent model'],
         ];
     }
 
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return array(
+            ['fillable', null, InputOption::VALUE_OPTIONAL, 'Fillable attributes for the model'],
+            ['path', null, InputOption::VALUE_OPTIONAL, 'Where should the file be created?'],
+            ['templatePath', null, InputOption::VALUE_OPTIONAL, 'The location of the template for this generator'],
+            ['testing', null, InputOption::VALUE_OPTIONAL, 'For internal use only.']
+        );
+    }
 }
--- src/config/config.php
+++ src/config/config.php
@@ -29,9 +29,9 @@ return [
     |--------------------------------------------------------------------------
     |
     */
-    'model_target_path'   => app_path(),
+    'model_target_path'   => app_path('models'),
 
-    'controller_target_path'   => app_path('Http/Controllers'),
+    'controller_target_path'   => app_path('controllers'),
 
     'migration_target_path'   => base_path('database/migrations'),
 
